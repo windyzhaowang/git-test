3de6b963b944d1be89ed51fba6e4e047abf5f652
zhaowang <a1244128570@gmail.com>
2018-09-17 20:47:54 +0800

    aa

---
+++ untitled114/src/com/itheima/git/A.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+        System.out.println("Hello git project");
+    }
+}
